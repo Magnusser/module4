6d9a30aef0d33e6d6db6fc05c549da32af1fe628
Magnusser <magnus.roland.raadla@gmail.com>
2024-12-13 13:53:32 +0200

    added customs

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,21 @@ export default {
     ],
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+          colors: {
+            'primary': '#590C2E',
+            'secondary': '#151515',
+            'tritary': '#300417',
+            'attention': '#F3E116',
+            '2ndary': '#232323',
+            'bomb': '#CDBD0E'
+          },
+          fontFamily: {
+            primary: ['Koulen', 'sans']
+          },
+          dropShadow: {
+            'text': '0 -10px 0px rgba(0, 0, 0, 1)'
+          }
         },
-    },
+      },
     plugins: [],
 };
